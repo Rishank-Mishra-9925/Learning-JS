f0e3213809c9c90281edb8ef5504965e5a013f93
Rishank-Mishra-9925 <83667169+Rishank-Mishra-9925@users.noreply.github.com>
2023-12-18 12:15:20 +0000

    Data Types practice.

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Rishank")
+
+
+let name = "mishra"
+let age = 15
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined, because it is a seperate data type
+console.log(typeof null); // object, because null is treated as object
